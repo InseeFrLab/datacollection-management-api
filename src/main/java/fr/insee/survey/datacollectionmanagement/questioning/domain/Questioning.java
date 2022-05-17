5d4c19932ef9f6b9fc8aef74d9cdbf11b60ee36d
Eric Thuaud <eric.thuaud@insee.fr>
2022-05-17 16:26:44 +0200

    add forgotten classes uml

---
+++ src/main/java/fr/insee/survey/datacollectionmanagement/questioning/domain/Questioning.java
@@ -0,0 +1,28 @@
+package fr.insee.survey.datacollectionmanagement.questioning.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Set;
+
+@Entity
+@Data
+public class Questioning {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String modelName;
+
+    @OneToMany
+    private Set<QuestioningEvent> questioningEvents;
+
+    @OneToMany
+    private Set<QuestioningAccreditation> questioningAccreditations;
+
+
+}
