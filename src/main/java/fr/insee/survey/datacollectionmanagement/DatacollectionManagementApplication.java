4344dbb7f6512f25fa3e97c7aec4a21b71496345
Benjamin Bour <benjamin.bour@insee.fr>
2022-05-17 10:54:31 +0200

    First commit

---
+++ src/main/java/fr/insee/survey/datacollectionmanagement/DatacollectionManagementApplication.java
@@ -0,0 +1,13 @@
+package fr.insee.survey.datacollectionmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatacollectionManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatacollectionManagementApplication.class, args);
+	}
+
+}
