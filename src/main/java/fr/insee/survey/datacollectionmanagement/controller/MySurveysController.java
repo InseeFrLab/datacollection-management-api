ceb3a4f90834d98e655ffe5ee11b5f3e29abc551
BettyB979 <betty.becuwe@insee.fr>
2022-05-20 15:14:38 +0200

    Try fix cors issue

--- src/main/java/fr/insee/survey/datacollectionmanagement/controller/MySurveysController.java
+++ src/main/java/fr/insee/survey/datacollectionmanagement/controller/MySurveysController.java
@@ -2,10 +2,10 @@ package fr.insee.survey.datacollectionmanagement.controller;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +19,10 @@ import fr.insee.survey.datacollectionmanagement.questioning.domain.QuestioningAc
 import fr.insee.survey.datacollectionmanagement.questioning.repository.QuestioningAccreditationRepository;
 
 @RestController
+@CrossOrigin
 public class MySurveysController {
 
-    private static final String STROMAE_URL = "https://stromae-v2.dev.insee.io/.../questionnaire/";
+    private static final String STROMAE_URL = "https://dev.insee.io/questionnaire/";
 
     @Autowired
     private QuestioningAccreditationRepository questioningAccreditationRepository;
@@ -32,9 +33,6 @@ public class MySurveysController {
     @GetMapping(value = "mySurveys/{id}")
     public List<MySurveyDto> findById(@PathVariable("id") String id) {
         
-
-        // 2021-03-24 16:48:05.591
-        Date date = new Date();
         List<MySurveyDto> listSurveys = new ArrayList<>();
         List<QuestioningAccreditation> accreditations = questioningAccreditationRepository.findByIdContact(id);
 
