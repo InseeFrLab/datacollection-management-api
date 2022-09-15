ae691c7abf22bb0de81c0298d0f82ab3c25e87ec
BettyB979 <betty.becuwe@insee.fr>
2022-09-15 10:06:28 +0200

    Fix build war

--- src/main/java/fr/insee/survey/datacollectionmanagement/DatacollectionManagementApplication.java
+++ src/main/java/fr/insee/survey/datacollectionmanagement/DatacollectionManagementApplication.java
@@ -2,12 +2,18 @@ package fr.insee.survey.datacollectionmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DatacollectionManagementApplication {
+public class DatacollectionManagementApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DatacollectionManagementApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DatacollectionManagementApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DatacollectionManagementApplication.class);
+    }
 }
