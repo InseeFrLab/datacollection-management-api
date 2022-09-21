1a087e90eeffbbe44c6f89f5500eeb4db62c0392
Eric Thuaud <eric.thuaud@insee.fr>
2022-09-21 10:54:17 +0200

    attempt to fix swagger issue

--- src/main/java/fr/insee/survey/datacollectionmanagement/DatacollectionManagementApplication.java
+++ src/main/java/fr/insee/survey/datacollectionmanagement/DatacollectionManagementApplication.java
@@ -1,10 +1,17 @@
 package fr.insee.survey.datacollectionmanagement;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "/", description = "Default Server URL")
+        }
+)
 @SpringBootApplication
 public class DatacollectionManagementApplication extends SpringBootServletInitializer {
 
