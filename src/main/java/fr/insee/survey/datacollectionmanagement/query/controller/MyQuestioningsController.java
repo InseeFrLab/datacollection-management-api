ce3f50a2e9960763c8a55bb00cd240e4ef7024af
Eric Thuaud <eric.thuaud@insee.fr>
2023-03-06 18:20:31 +0100

    try to correct redirection

--- src/main/java/fr/insee/survey/datacollectionmanagement/query/controller/MyQuestioningsController.java
+++ src/main/java/fr/insee/survey/datacollectionmanagement/query/controller/MyQuestioningsController.java
@@ -2,8 +2,15 @@ package fr.insee.survey.datacollectionmanagement.query.controller;
 
 import java.util.List;
 
+import fr.insee.survey.datacollectionmanagement.config.ApplicationConfig;
+import fr.insee.survey.datacollectionmanagement.config.auth.user.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +25,15 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 @Tag(name = "4 - Cross domain")
 public class MyQuestioningsController {
+    static final Logger LOGGER = LoggerFactory.getLogger(MyQuestioningsController.class);
+
+
 
     @Autowired
     private MySurveysService mySurveysService;
+
+    @Autowired
+    ApplicationConfig config;
 
     @GetMapping(value = Constants.API_MY_QUESTIONINGS_ID)
     @PreAuthorize("@AuthorizeMethodDecider.isInternalUser() "
@@ -28,6 +41,22 @@ public class MyQuestioningsController {
             + "|| @AuthorizeMethodDecider.isRespondent()"
             + "|| @AuthorizeMethodDecider.isAdmin() ")
     public List<MyQuestioningDto> findById(HttpServletRequest request) {
+
+
+        String idec=null;
+
+        if (config.getAuthType().equals("OIDC")) {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            final Jwt jwt = (Jwt) authentication.getPrincipal();
+            idec=jwt.getClaimAsString(config.getIdClaim());
+            LOGGER.info("jwt claim is "+idec);
+        }
+        else{
+           idec="anonymous";
+        }
+        LOGGER.info("Userprincipal name is " +request.getUserPrincipal().getName().toUpperCase());
+
+        LOGGER.info("remote user is "+request.getRemoteUser().toUpperCase());
 
         List<MyQuestioningDto> listSurveys = mySurveysService.getListMySurveys(request.getRemoteUser().toUpperCase());
 
